cb1ecc368d2f842735e86da8418dbceb1c628d3b
Yeffercito <yeffermlopez@gmail.com>
2023-02-06 13:37:56 -0500

    mision 3

--- Mision/Mision 3/mision3.js
+++ Mision/Mision 3/mision3.js
@@ -50,7 +50,7 @@ do {
             do {
               numero = parseInt(prompt("Ingrese la cantidad de estudiantes"));
               if (numero <= 0 || isNaN(numero)) {
-                console.log("Ingrese un valor superior o igual a cero");
+                console.log("Ingrese un valor superior a cero");
               }
             } while (numero <= 0 || isNaN(numero));
             console.log(
@@ -77,162 +77,185 @@ do {
       valid = 0;
       if (numero == 0) {
         alert("No hay estudiantes");
-      }
-      if (numero === nombres.length) {
-        do {
-          cambio1 = parseInt(
-            prompt(
-              "¿Seguro que quiere volver a registrar los datos de los estudiantes?\n(Todos los valores dados se perderan)" +
-                "\nIngrese: \n'1' para volver a registrar \n '2' para proseguir con los mismos datos"
-            )
-          );
-          if (cambio1 === 1) {
-            console.log("Proceda a cambiar los datos del estudiante ");
-            nombres = [];
-            codigos = [];
-            niveles = [];
-          } else if (cambio1 === 2) {
-            console.log("Sus datos siguen intactos");
-            valid = 1;
-          }
-        } while (cambio1 > 2 || cambio1 < 1);
-      }
-
-      if (valid == 0) {
-        for (let i = 0; i < numero; i++) {
-          nombre = prompt("Ingrese su nombre");
-          nombres.push(nombre);
+      } else {
+        if (numero === nombres.length) {
           do {
-            a = 1;
-            codigo = prompt(
-              "Ingrese su codigo " + nombre + ", (recuerde que es de 2 digitos)"
-            );
-            if (codigo.length < 2 || codigo.length > 2 || isNaN(codigo)) {
-              console.log("Ingrese un valor de dos digitos");
-            }
-            if (codigos.length > 0) {
-              codigos.forEach((e) => {
-                if (e === codigo) {
-                  alert("Su codigo esta repetido, Ingrese otro ");
-                  a = 0;
-                }
-              });
-            } else {
-              codigos.push(codigo);
-            }
-          } while (
-            codigo.length < 2 ||
-            codigo.length > 2 ||
-            isNaN(codigo) ||
-            a === 0
-          );
-          do {
-            nivel = parseInt(
+            cambio1 = parseInt(
               prompt(
-                "Ingrese \n '1'si su nivel es 'Beginner'\n'2'si su nivel es 'junior'\n'3'si su nivel es 'senior'"
+                "¿Seguro que quiere volver a registrar los datos de los estudiantes?\n(Todos los valores dados se perderan)" +
+                  "\nIngrese: \n'1' para volver a registrar \n '2' para proseguir con los mismos datos"
               )
             );
-            if (nivel === 1) {
-              niveles.push("Beginner");
-            } else if (nivel === 2) {
-              niveles.push("Junior");
-            } else if (nivel === 3) {
-              niveles.push("Senior");
-            } else {
-              nivel = 0;
+            if (cambio1 === 1) {
+              console.log("Proceda a cambiar los datos del estudiante ");
+              nombres = [];
+              codigos = [];
+              niveles = [];
+            } else if (cambio1 === 2) {
+              console.log("Sus datos siguen intactos");
+              valid = 1;
             }
-          } while (nivel == 0);
+          } while (cambio1 > 2 || cambio1 < 1);
+        }
+
+        if (valid == 0) {
+          for (let i = 0; i < numero; i++) {
+            nombre = prompt("Ingrese su nombre");
+            nombres.push(nombre);
+            do {
+              a = 1;
+              codigo = prompt(
+                "Ingrese su codigo " +
+                  nombre +
+                  ", (recuerde que es de 2 digitos)"
+              );
+              if (codigo.length < 2 || codigo.length > 2 || isNaN(codigo)) {
+                console.log("Ingrese un valor de dos digitos");
+              }
+              if (codigos.length > 0) {
+                codigos.forEach((e) => {
+                  if (e === codigo) {
+                    alert("Su codigo esta repetido, Ingrese otro ");
+                    a = 0;
+                  }
+                });
+              } else {
+                codigos.push(codigo);
+              }
+            } while (
+              codigo.length < 2 ||
+              codigo.length > 2 ||
+              isNaN(codigo) ||
+              a === 0
+            );
+            do {
+              nivel = parseInt(
+                prompt(
+                  "Ingrese \n '1'si su nivel es 'Beginner'\n'2'si su nivel es 'junior'\n'3'si su nivel es 'senior'"
+                )
+              );
+              if (nivel === 1) {
+                niveles.push("Beginner");
+              } else if (nivel === 2) {
+                niveles.push("Junior");
+              } else if (nivel === 3) {
+                niveles.push("Senior");
+              } else {
+                nivel = 0;
+              }
+            } while (nivel == 0);
+          }
         }
       }
 
       break;
     case 3:
-      for (let i = 0; i < numero; i++) {
-        y = crearObjeto(nombres[i], codigos[i], niveles[i], promedios[i]);
-        console.log(y);
+      if (numero == 0) {
+        alert("No se ha ingresado una cantidad de estudiantes");
+      } else {
+        for (let i = 0; i < numero; i++) {
+          y = crearObjeto(nombres[i], codigos[i], niveles[i], promedios[i]);
+          console.log(y);
+        }
       }
       break;
     case 4:
       valid1 = 0;
-
-      if (numero === promedios.length) {
-        do {
-          cambio2 = parseInt(
-            prompt(
-              "¿Seguro que quiere volver a registrar las notas de los estudiantes?\n(Todos los valores dados se perderan)" +
-                "\nIngrese: \n'1' para volver a registrar \n '2' para proseguir con los mismos datos"
-            )
-          );
-          if (cambio2 === 1) {
-            console.log("Proceda a cambiar los datos del estudiante ");
-            promedios = [];
-          } else if (cambio2 === 2) {
-            console.log("Sus datos siguen intactos");
-            valid = 1;
+      if (numero == 0) {
+        alert("No se ha ingresado una cantidad de estudiantes");
+      } else {
+        if (numero === promedios.length) {
+          do {
+            cambio2 = parseInt(
+              prompt(
+                "¿Seguro que quiere volver a registrar las notas de los estudiantes?\n(Todos los valores dados se perderan)" +
+                  "\nIngrese: \n'1' para volver a registrar \n '2' para proseguir con los mismos datos"
+              )
+            );
+            if (cambio2 === 1) {
+              console.log("Proceda a cambiar los datos del estudiante ");
+              promedios = [];
+            } else if (cambio2 === 2) {
+              console.log("Sus datos siguen intactos");
+              valid = 1;
+            }
+          } while (cambio2 > 2 || cambio2 < 1);
+        }
+        if (valid == 0 && numero == nombres.length) {
+          for (let i = 0; i < numero; i++) {
+            do {
+              nota1 = parseFloat(
+                prompt("Ingrese la primera nota de " + nombres[i])
+              );
+              if (nota1 < 0 || nota1 > 5 || isNaN(nota1)) {
+                console.log(
+                  "Ingrese un valor superior a cero y menor que cinco"
+                );
+              }
+            } while (nota1 < 0 || nota1 > 5 || isNaN(nota1));
+            do {
+              nota2 = parseFloat(
+                prompt("Ingrese la segunda nota de " + nombres[i])
+              );
+              if (nota2 < 0 || nota2 > 5 || isNaN(nota2)) {
+                console.log(
+                  "Ingrese un valor superior a cero y menor que cinco"
+                );
+              }
+            } while (nota2 < 0 || nota2 > 5 || isNaN(nota2));
+            do {
+              nota3 = parseFloat(
+                prompt("Ingrese la tercera nota de " + nombres[i])
+              );
+              if (nota3 < 0 || nota3 > 5 || isNaN(nota3)) {
+                console.log(
+                  "Ingrese un valor superior a cero y menor que cinco"
+                );
+              }
+            } while (nota3 < 0 || nota3 > 5 || isNaN(nota3));
+            promedio = (nota1 + nota2 + nota3) / 3;
+            promedios.push(promedio);
           }
-        } while (cambio2 > 2 || cambio2 < 1);
-      }
-      if (valid == 0) {
-        for (let i = 0; i < numero; i++) {
-          do {
-            nota1 = parseFloat(
-              prompt("Ingrese la primera nota de " + nombres[i])
-            );
-            if (nota1 < 0 || nota1 > 5 || isNaN(nota1)) {
-              console.log("Ingrese un valor superior a cero y menor que cinco");
-            }
-          } while (nota1 < 0 || nota1 > 5 || isNaN(nota1));
-          do {
-            nota2 = parseFloat(
-              prompt("Ingrese la segunda nota de " + nombres[i])
-            );
-            if (nota2 < 0 || nota2 > 5 || isNaN(nota2)) {
-              console.log("Ingrese un valor superior a cero y menor que cinco");
-            }
-          } while (nota2 < 0 || nota2 > 5 || isNaN(nota2));
-          do {
-            nota3 = parseFloat(
-              prompt("Ingrese la tercera nota de " + nombres[i])
-            );
-            if (nota3 < 0 || nota3 > 5 || isNaN(nota3)) {
-              console.log("Ingrese un valor superior a cero y menor que cinco");
-            }
-          } while (nota3 < 0 || nota3 > 5 || isNaN(nota3));
-          promedio = (nota1 + nota2 + nota3) / 3;
-          promedios.push(promedio);
+        } else if (!(numero == nombres.length)) {
+          alert("No se han resgitrado datos de los estudiantes");
         }
       }
       break;
     case 5:
-      for (let i = 0; i < numero; i++) {
-        if (promedios[i] >= 3.5) {
+      if (numero == 0) {
+        alert("No se ha ingresado una cantidad de estudiantes");
+      } else {
+        for (let i = 0; i < numero; i++) {
+          if (promedios[i] >= 3.5) {
+            console.log(
+              "El estudiante " +
+                nombres[i] +
+                " aprobo con un promedio de: " +
+                promedio
+            );
+          } else {
+            console.log(
+              "El estudiante " +
+                nombres[i] +
+                " reprobo con un promedio de: " +
+                promedio
+            );
+          }
+        }
+
+        promedios.forEach((e) => {
+          sum_prom = e + sum_prom;
+        });
+        let prom = sum_prom / promedios.length;
+        if (prom >= 3.5) {
           console.log(
-            "El estudiante " +
-              nombres[i] +
-              " aprobo con un promedio de: " +
-              promedio
+            "Los estudiantes del curso 'aprobaron' con un promedio de: " + prom
           );
         } else {
           console.log(
-            "El estudiante " +
-              nombres[i] +
-              " reprobo con un promedio de: " +
-              promedio
+            "Los estudiantes del curso 'reprobaron' con un promedio de: " + prom
           );
         }
-      }
-      promedios.forEach((e) => {
-        sum_prom = e + sum_prom;
-      });
-      let prom = sum_prom / promedios.length;
-      if (prom >= 3.5) {
-        console.log(
-          "Los estudiantes del curso 'aprobaron' con un promedio de: " + prom
-        );
-      } else {
-        console.log(
-          "Los estudiantes del curso 'reprobaron' con un promedio de: " + prom
-        );
       }
       break;
     case 0:
